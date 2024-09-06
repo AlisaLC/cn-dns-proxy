8117907603e98e8494c9aa761d19cfaea3844325
AlisaLC <ali_sal1381@yahoo.com>
2024-09-06 17:57:51 +0330

    changed dns payload

--- client.py
+++ client.py
@@ -26,28 +26,18 @@ def wrap_in_dns(packet):
     ip_layer = IP(packet)
     if ip_layer.proto != 6:
         return
-    dns_packet = DNS(
-        id=0xAAAA,  # Random transaction ID
-        qr=1,  # This is a response
-        opcode=0,  # Standard query
-        aa=1,  # Authoritative answer
-        rd=1,  # Recursion desired
-        ra=1,  # Recursion available
-        z=0,
-        rcode=0,  # No error
-        qd=DNSQR(qname="sharif.edu", qtype="A", qclass="IN"),  # A simple DNS query
-        an=DNSRR(rrname="sharif.edu", rdata="192.0.2.1"),  # DNS Answer
-    )
-
-    # Create an EDNS option record (OPT) with the raw TCP packet as the RData
     opt_record = DNSRROPT(
-        rrname='.',
-        rclass=4096,  # Requestor's UDP payload size (e.g., 4096 bytes)
-        rdata=packet  # TCP packet stored in the RData field of the OPT record
+        rclass=4096,  # UDP payload size
+        rdlen=len(packet) + 4,  # Length of the option data plus option header
+        rdata=[EDNS0TLV(optcode=65001, optlen=len(packet), optdata=packet)]
     )
-
-    # Add the OPT record to the DNS packet
-    dns_packet.ar = opt_record
+    dns_packet = DNS(
+        id=random.getrandbits(16),
+        opcode=0,
+        rcode=0,
+        qd=DNSQR(qname="sharif.edu", qtype="A", qclass="IN"),
+        ar=opt_record,
+    )
 
     return dns_packet
     
