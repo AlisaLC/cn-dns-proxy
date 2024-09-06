fe28c70b4e6661d702b0343562ab942c68606977
AlisaLC <ali_sal1381@yahoo.com>
2024-09-06 16:29:06 +0330

    added .env

--- client.py
+++ client.py
@@ -2,34 +2,29 @@ import os
 import fcntl
 import struct
 import subprocess
+from dotenv import load_dotenv
+load_dotenv()
 
 TUNSETIFF = 0x400454ca
 IFF_TUN = 0x0001
 IFF_NO_PI = 0x1000
 
-# Open the TUN device file
 tun = os.open('/dev/net/tun', os.O_RDWR)
 
-# Configure the TUN interface
-ifr = struct.pack('16sH', b'dprox', IFF_TUN | IFF_NO_PI)
+TUN_NAME = os.getenv('TUN_NAME')
+
+ifr = struct.pack('16sH', TUN_NAME, IFF_TUN | IFF_NO_PI)
 fcntl.ioctl(tun, TUNSETIFF, ifr)
 
-# Bring up the interface (using subprocess to run ifconfig/ip commands)
-subprocess.run(['ip', 'addr', 'add', '172.16.0.2/24', 'dev', 'dprox'])
-subprocess.run(['ip', 'link', 'set', 'dev', 'dprox', 'up'])
+subprocess.run(['ip', 'addr', 'add', os.getenv('SUBNET'), 'dev', TUN_NAME])
+subprocess.run(['ip', 'link', 'set', 'dev', TUN_NAME, 'up'])
 
-print("TUN interface 'dprox' is up and running.")
+print(f"TUN interface '{TUN_NAME}' is up and running.")
 
-# Reading and writing to the TUN interface
 try:
     while True:
-        # Reading data from TUN interface
-        packet = os.read(tun, 2048)
+        packet = os.read(tun, 1500)
         print(f'Received packet: {packet}')
-
-        # Writing data to the TUN interface
-        response = b'Hello, world!'  # Example packet
-        os.write(tun, response)
 except KeyboardInterrupt:
     print("Shutting down TUN interface.")
 finally:
